ac472ba9bb7bef9061e9e40f5ccdd3f3ce375ba9
kishoremohan29 <55447556+kishoremohan29@users.noreply.github.com>
2022-11-02 18:20:43 +0530

    Add files via upload

---
+++ 6/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace CC6
+{
+    public class Movie
+    {
+        private static int s_Id = 0;
+        public int Id { get; }
+        public string Title { get; set; }
+        public string Cast { get; set; }
+        public double TicketPrice { get; set; }
+
+        public Movie(string title, string cast, double ticketPrice)
+        {
+            s_Id++;
+            Id = s_Id;
+            Title = title;
+            Cast = cast;
+            TicketPrice = ticketPrice;
+        }
+    }
+}
---
+++ 6/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using CC6;
+namespace CC6
+{
+    public class Ticket
+    {
+        public Movie Movie { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int TicketCount { get; set; }
+        public double TotalAmount { get; set; }
+
+        public Ticket(Movie movie, DateTime date, int ticketCount, double totalAmount)
+        {
+            Movie = movie;
+            BookingDate = date;
+            TicketCount = ticketCount;
+            TotalAmount = totalAmount;
+        }
+    }
+}
