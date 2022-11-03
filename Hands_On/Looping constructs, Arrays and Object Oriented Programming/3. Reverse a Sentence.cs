fb3a7e0e26109efb80b40be5417438760abfb8fd
kishoremohan29 <55447556+kishoremohan29@users.noreply.github.com>
2022-11-04 01:58:09 +0530

    Add files via upload

---
+++ Hands_On/Looping constructs, Arrays and Object Oriented Programming/3. Reverse a Sentence.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Program      
+{
+    //implement code here
+    public static void Main(string[] args)
+    {
+        string sentence, reverse="";
+        
+        Console.WriteLine("Enter a string");
+        sentence = Console.ReadLine();
+        
+        string[] temp = sentence.Split();
+        
+        for(int i = temp.Length-1; i >= 0; i--)
+        {
+            reverse += temp[i] + " ";
+        }
+        Console.WriteLine(reverse);
+    }
+}
