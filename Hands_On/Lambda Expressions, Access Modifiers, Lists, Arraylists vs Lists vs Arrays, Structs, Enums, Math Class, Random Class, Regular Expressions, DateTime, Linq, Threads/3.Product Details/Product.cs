d862498786dcab12bc1cc2388b17727460a92352
kishoremohan29 <55447556+kishoremohan29@users.noreply.github.com>
2022-11-04 02:34:04 +0530

    Update and rename Hands_On/Lambda Expressions, Access Modifiers, Lists, Arraylists vs Lists vs Arrays, Structs, Enums, Math Class, Random Class, Regular Expressions, DateTime, Linq, Threads/3.Product Details.cs to Hands_On/Lambda Expressions, Access Modifiers, Lists, Arraylists vs Lists vs Arrays, Structs, Enums, Math Class, Random Class, Regular Expressions, DateTime, Linq, Threads/3.Product Details/Product.cs

--- Hands_On/Lambda Expressions, Access Modifiers, Lists, Arraylists vs Lists vs Arrays, Structs, Enums, Math Class, Random Class, Regular Expressions, DateTime, Linq, Threads/3.Product Details/Product.cs
+++ Hands_On/Lambda Expressions, Access Modifiers, Lists, Arraylists vs Lists vs Arrays, Structs, Enums, Math Class, Random Class, Regular Expressions, DateTime, Linq, Threads/3.Product Details/Product.cs
@@ -1,4 +1,3 @@
-***********Product**********
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -46,43 +45,8 @@ public class Product
     }
 }
 
-
-********Program**********
-
+****
 
 
-using System;
-using System.Collections;
-using System.Globalization;
-using System.IO;
 
 
-public class Program  
-{
-    public static void Main(string[] args) 
-    {
-
-        
-        StreamReader reader = new StreamReader("input.csv");
-        string data = "";
-        Product p = null;
-        ArrayList arlobj = new ArrayList();
-        while ((data = reader.ReadLine()) != null)
-        {
-            string[] info = data.Split(',');
-            string productName = info[0].Trim();
-            string serialNumber = info[1].Trim();
-            DateTime purchaseDate = DateTime.ParseExact(info[2].Trim(), "dd-MM-yyyy", null);
-            double cost = Double.Parse(info[3].Trim());
-            p = new Product(productName, serialNumber, purchaseDate, cost);
-            arlobj.Add(p);
-
-        }
-        Console.WriteLine("{0}", String.Format("{0,-15}{1,-15}{2,-15}{3,-15}", "Product Name", "Serial Number", "Purchase Date", "Cost"));
-        foreach (var obj in arlobj)
-        {
-            Product prd = (Product)obj;
-            Console.WriteLine("{0}", prd.ToString());
-        }
-    }
-}
